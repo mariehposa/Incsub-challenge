0f35942b4cf25c6f60a8189784dbb611a1525630
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-01-25 16:43:38 +0100

    add function to reveal and hide password

--- signup.js
+++ signup.js
@@ -1,3 +1,16 @@
+//Function to toggle reveal and hide password
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.forms["signupForm"]["password"];
+
+togglePassword.addEventListener("click", function (e) {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  this.classList.toggle("fa-eye-slash");
+  this.classList.toggle("fa-eye");
+});
+
+//Function to validate form
 function validateForm() {
   var email = document.forms["signupForm"]["email"].value;
   var password = document.forms["signupForm"]["password"].value;
